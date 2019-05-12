a0b67239a4229e2349c6f2bb8213624e8290b46d
VEWOXIC <vewoxic@foxmail.com>
2019-05-12 16:41:24 +0800

    ?

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from func import *
 import blueserial
+import time
 #text1=input('input text1')
 #text2=input('input text2')
-image=bmpmaker('congratulation','')
-byteslist=framemaker(image)
-blueserial.sendframe(byteslist)
+wordimage=word_bmpmaker('test2')
+
+byteslist=framemaker(wordimage)
+blueserial.load_frame(byteslist)
+time.sleep(0.05)
+blueserial.screen_show()
